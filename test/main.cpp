4e9dfc18972c7b31718dae2fe376ca823d0f108a
dearfriday <dream2053@vip.qq.com>
2019-05-12 22:20:49 +0800

    1111

--- test/main.cpp
+++ test/main.cpp
@@ -1,47 +1,48 @@
-#include <stdlib.h>
+#include <type_traits>
 #include <iostream>
-using namespace std;
 
-struct test
+struct S {
+    double operator()(char, int&);
+    float operator()(int) { return 1.0;}
+};
+
+template<class T>
+typename std::result_of<T(int)>::type f(T& t)
 {
-    int m_i;
-    test( int i ) : m_i(i){}
-    test()
-    {
-        test(1);
-    }
-};
+    std::cout << "overload of f for callable T\n";
+    return t(0);
+}
 
-
-
-template <typename T, bool>
-struct get_size;
-
-template <typename T>
-struct get_size<T, false>{
-    union any{
-        any(){}
-        ~any(){}
-        T t;
-    } _value;
-};
-
-
-template <typename T>
-struct get_size<T, true> : private T{
-
-};
-
-
-typedef  get_size<test, true>   true_size;
-typedef  get_size<test, false>  true_size2;
-
-
+template<class T, class U>
+int f(U u)
+{
+    std::cout << "overload of f for non-callable T\n";
+    return u;
+}
 
 int main()
 {
-    std::cout << sizeof(true_size) << std::endl;
-    std::cout << sizeof(true_size2) << std::endl;
+    // the result of invoking S with char and int& arguments is double
+//    std::result_of<S(char, int&)>::type d = 3.14; // d has type double
+//    static_assert(std::is_same<decltype(d), double>::value, "1111");
+//
+//    // the result of invoking S with int argument is float
+//    std::result_of<S(int)>::type x = 3.14; // x has type float
+//    static_assert(std::is_same<decltype(x), float>::value, "2222");
+//
+//    // result_of can be used with a pointer to member function as follows
+//    struct C { double Func(char, int&); };
+//    std::result_of<decltype(&C::Func)(C, char, int&)>::type g = 3.14;
+//    static_assert(std::is_same<decltype(g), double>::value, "333");
+//
+//    f<C>(1); // may fail to compile in C++11; calls the non-callable overload in C++14
+//    std::function<double(int)> f = [](int v){return 0.1f;};
+//
+//    std::result_of<decltype(f)>::value k;
 
-    return 0;
+
+
+
+
+
 }
